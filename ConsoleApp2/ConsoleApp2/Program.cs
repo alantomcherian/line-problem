0aeaa66ba34731a110dae4dae3327853a5cb3027
alantomcherian <atcherian101@gmail.com>
2021-08-03 19:25:07 +0530

    feat:uc1-Length of a line

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -10,13 +10,13 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Console.WriteLine("enter coordinates of line (x1) ::");
-            int x1 = Convert.ToInt32(Console.ReadLine());
+            int x1 = int.Parse(Console.ReadLine());
             Console.WriteLine("enter coordinates of line (y1) ::");
-            int y1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = int.Parse(Console.ReadLine());
             Console.WriteLine("enter coordinates of line (x2) ::");
-            int x2 = Convert.ToInt32(Console.ReadLine());
+            int x2 = int.Parse(Console.ReadLine());
             Console.WriteLine("enter coordinates of line (y2) ::");
-            int y2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = int.Parse(Console.ReadLine());
             float l1 = (float)Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
             Console.WriteLine(l1);
         }
