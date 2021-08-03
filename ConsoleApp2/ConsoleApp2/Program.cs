f4c2aff34a17028089ab71a7a19e3c862b7ba6c4
alantomcherian <atcherian101@gmail.com>
2021-08-03 19:14:52 +0530

    feat:uc1-Length of a line

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -3,10 +3,22 @@
 namespace ConsoleApp2
 {
     class Program
-    {
+    {/// <summary>
+     /// 
+     /// </summary>
+     /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            Console.WriteLine("enter coordinates of line (x1) ::");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter coordinates of line (y1) ::");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter coordinates of line (x2) ::");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter coordinates of line (y2) ::");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            float l1 = (float)Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            Console.WriteLine(l1);
         }
     }
 }
